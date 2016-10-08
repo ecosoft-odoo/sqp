a0201d0d42c27eb157df7c8861e53a46ad1b4e76
Kitti U <kittiu@gmail.com>
2016-10-08 23:56:08 +0700

    [ADD] ext_invoice_non_standard

---
+++ ext_invoice_non_standard/__openerp__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Set a flag "Non-Standard" on Invoice',
+    'author': 'Kitti U.',
+    'website': 'http://www.ecosoft.co.th',
+    'description': """
+For invoice with products in group Non-Standard (000005) and Export (000006)
+Mark Non-Standard flag. It will be exclude from Export Invoice to Winspeed
+    """,
+    'images': [],
+    'depends': ['account', 'ext_product'],
+    'category': 'Tools',
+    'demo': [],
+    'data': [],
+    'auto_install': False,
+    'installable': True,
+}
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
