99464ace794cfdab73c4187c051e16ee69979b39
Kitti U <kittiu@gmail.com>
2016-12-15 14:33:59 +0700

    -

--- sqp_winspeed_model/soinvoice.py
+++ sqp_winspeed_model/soinvoice.py
@@ -226,7 +226,7 @@ from account_invoice ai
                   ORDER BY name DESC LIMIT 1)
 
 where ai.type in ('out_invoice', 'out_refund') and (ail.is_deposit = false and ail.is_advance = false)
-    and ai.state not in ('draft', 'cancel') and ai.non_standard = false
+    and ai.state not in ('draft', 'cancel') and ai.non_standard is not true
 order by ai.id desc
         )""")
 
