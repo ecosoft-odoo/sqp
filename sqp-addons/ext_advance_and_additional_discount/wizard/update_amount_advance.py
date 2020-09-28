83160be98326e5a556356de88f6179f976c459aa
newtratip <tharathip.chaweewongphan@gmail.com>
2020-09-29 00:40:37 +0700

    [7.0][UPD] ext_advance_and_additional_discount

--- sqp-addons/ext_advance_and_additional_discount/wizard/update_amount_advance.py
+++ sqp-addons/ext_advance_and_additional_discount/wizard/update_amount_advance.py
@@ -12,6 +12,11 @@ class update_amount_advance(osv.osv_memory):
             string="Advance Amt",
             required=True,
             digits_compute=dp.get_precision("Account"),
+        ),
+        "tax_line": fields.one2many(
+            "update.account.invoice.tax",
+            "update_advance_id",
+            string="Tax Line",
         )
     }
 
@@ -21,7 +26,14 @@ class update_amount_advance(osv.osv_memory):
         active_id = context.get("active_id")
         invoice = self.pool.get("account.invoice").browse(
             cr, uid, active_id, context=context)
-        res["amount_advance"] = invoice.amount_advance
+        res.update({
+            "amount_advance": invoice.amount_advance,
+            "tax_line": [
+                (0, 0, {
+                    "tax_id": tax.id,
+                    "base": tax.base,
+                    "amount": tax.amount}) for tax in invoice.tax_line]
+        })
         return res
 
     def update_amount_advance(self, cr, uid, ids, context=None):
@@ -29,15 +41,67 @@ class update_amount_advance(osv.osv_memory):
         invoice = self.pool.get("account.invoice").browse(
             cr, uid, active_id, context=context)
         wizards = self.browse(cr, uid, ids, context=context)
-        diff_amount_advance = \
-            wizards[0].amount_advance - invoice.amount_advance
+        # Advance Amt
+        amount_advance = wizards[0].amount_advance
+        # Before Taxes
+        amount_beforetax = invoice.amount_net - amount_advance - \
+            invoice.amount_deposit
+        # Tax
+        amount_tax = sum([line.amount for line in wizards[0].tax_line])
+        # Before Retention
+        amount_beforeretention = amount_beforetax + amount_tax
+        # Total
+        amount_total = amount_beforeretention - invoice.amount_retention
+        # Update invoice
         cr.execute("""
             update account_invoice
-            set amount_advance = %s, amount_beforetax = %s,
+            set amount_advance = %s, amount_beforetax = %s, amount_tax = %s,
                 amount_beforeretention = %s, amount_total = %s
             where id = %s
-        """, (wizards[0].amount_advance,
-              invoice.amount_beforetax - diff_amount_advance,
-              invoice.amount_beforeretention - diff_amount_advance,
-              invoice.amount_total - diff_amount_advance,
-              active_id))
+        """, (amount_advance, amount_beforetax, amount_tax,
+              amount_beforeretention, amount_total, active_id))
+        # Update tax line
+        cur_obj = self.pool.get('res.currency')
+        for line in wizards[0].tax_line:
+            base_amount = cur_obj.compute(
+                cr, uid, invoice.currency_id.id,
+                invoice.company_id.currency_id.id, line.base,
+                context={
+                    "date": invoice.date_invoice or
+                    fields.date.context_today(
+                        self, cr, uid, context=context)}, round=False)
+            tax_amount = cur_obj.compute(
+                cr, uid, invoice.currency_id.id,
+                invoice.company_id.currency_id.id, line.amount,
+                context={
+                    "date": invoice.date_invoice or
+                    fields.date.context_today(
+                        self, cr, uid, context=context)}, round=False)
+            cr.execute("""
+                update account_invoice_tax
+                set base = %s, base_amount = %s, amount = %s, tax_amount = %s
+                where id = %s
+            """, (line.base, base_amount, line.amount, tax_amount,
+                  line.tax_id.id))
+
+
+class update_account_invoice_tax(osv.osv_memory):
+    _name = "update.account.invoice.tax"
+
+    _columns = {
+        "tax_id": fields.many2one(
+            "account.invoice.tax",
+            string="Tax",
+        ),
+        "base": fields.float(
+            string="Base",
+            digits_compute=dp.get_precision("Account"),
+        ),
+        "amount": fields.float(
+            string="Amount",
+            digits_compute=dp.get_precision("Account"),
+        ),
+        "update_advance_id": fields.many2one(
+            "update.amount.advance",
+        )
+    }
